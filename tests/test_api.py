ee45d9b7d13f72d6cec636f3901545143b9f0519
dxphilo <developerphilo@gmail.com>
2023-06-15 15:17:25 +0300

    fix: failing tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,15 +7,16 @@ client = TestClient(app)
 
 sign_up_info = {
         "fullname":"John Philip",
-        "email":"john@gmail.com",
+        "email":"johnn@gmail.com",
         "password":"OkwonkowoNaKitambiUtawezana",
+        "role":"admin"
         }
 
 
 def test_health_check():
-    response = client.get('/')
+    response = client.get('/health')
     assert response.status_code == 200
-    assert response.json() == {"Message": "Server is up and running"}
+    assert response.json() == {"status": "ok 👍 "}
 
 def test_user_creation():
     response = client.post('/signup/',json=sign_up_info)
